13ea1478d24d2aa66a3206dae4f54637d95fdc2b
Lee Crosby <ligand@hotmail.co.uk>
2016-12-17 15:42:01 +0000

    Create orInclude.js

---
+++ reference/or/orInclude.js
@@ -0,0 +1,3 @@
+document.write("<script	type='text/JavaScript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js'></script>");
+document.write("<script	type='text/JavaScript' src='orConf.js'></script>");
+document.write("<script	type='text/JavaScript' src='orHelper.js'></script>");
